1bb5b3e4261828f497bdacdd28aefed7d880fce6
nivetha k <nivinataliya@gmail.com>
2020-08-09 19:38:19 +0530

    myportolio finished

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATICFILES_DIRS=[BASE_DIR / 'myportfolio/static']
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 
